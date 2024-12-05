16587aa46e1b1c1b485f4ddead614408f4635f46
Emir Boeev <emir.boeev@iMacK.local>
2024-12-05 10:42:52 +0100

    first commit

---
+++ DartsScoreBoard/DartsScoreBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  DartsScoreBoardApp.swift
+//  DartsScoreBoard
+//
+//  Created by Emir Boeev on 05/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct DartsScoreBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DartsScoreBoardTests/DartsScoreBoardTests.swift
@@ -0,0 +1,16 @@
+//
+//  DartsScoreBoardTests.swift
+//  DartsScoreBoardTests
+//
+//  Created by Emir Boeev on 05/12/2024.
+//
+
+import Testing
+
+struct DartsScoreBoardTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
